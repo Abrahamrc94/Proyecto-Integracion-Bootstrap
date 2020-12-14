8ac99649077bdd81c42110d3add39f1d13291caf
Abraham Romero <abrahamciclo@gmail.com>
2020-12-14 22:39:03 +0100

    Proyecto Integración con Bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'producto',
+    component: ProductoComponent,
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductoComponent,
+    ContactoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
